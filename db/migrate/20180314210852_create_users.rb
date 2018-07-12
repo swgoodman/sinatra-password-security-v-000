ceb715faff503d1c72dfc176c65628c7c1f5fde2
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-12 16:15:02 +0000

    automatically backed up by learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
     create_table :users do |t|
-    t.string :username
-    t.string :password_digest
+      t.string :username
+      t.string :password_digest
+    end
   end
 
   def down
